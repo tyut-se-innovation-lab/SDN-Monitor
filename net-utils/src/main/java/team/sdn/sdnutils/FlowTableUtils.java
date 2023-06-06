10f6b62d56e1f38ed5dce6d2bc0f5bb314c95630
steveevens <3236805730@qq.com>
2023-06-06 21:03:46 +0800

    to(Util):流表相关连接

--- net-utils/src/main/java/team/sdn/sdnutils/FlowTableUtils.java
+++ net-utils/src/main/java/team/sdn/sdnutils/FlowTableUtils.java
@@ -15,16 +15,18 @@ public class FlowTableUtils {
     /**
      * 为某个交换机下发流表
      * @param switchId 交换机id
+     * @param tableId 流表id
      * @param flow 流表内容
      * @return 是否下发成功
      */
-    public static boolean addFlowForSwitch(String switchId,OpenFlow flow){
-        return true;
+    public static boolean addFlowForSwitch(String switchId,String tableId,OpenFlow flow){
+        return HttpSender.post(Address.ADD_FLOW[0]+switchId+Address.ADD_FLOW[1]+tableId,flow);
     }
 
     /**
      * 获取某个交换器的某个流表的所有流表项
      * @param switchId 交换机id
+     * @param tableId 流表id
      * @return 流表们
      */
     public static String getAllFlowsOfTable(String switchId,String tableId){
@@ -32,23 +34,25 @@ public class FlowTableUtils {
     }
 
     /**
-     * 删除某个交换机的某个流表
+     * 删除某个交换机的某个流表项
      * @param switchId 交换机id
      * @param tableId 流表id
+     * @param flowId 流表项id
      * @return 是否成功删除
      */
-    public static boolean deleteFlowTable(String switchId,String tableId){
-        return true;
+    public static boolean deleteFlowTable(String switchId,String tableId,String flowId){
+        return HttpSender.delete(Address.DELETE_FLOW[0]+switchId+Address.DELETE_FLOW[1]+tableId+Address.DELETE_FLOW[2]+flowId);
     }
 
 
     /**
-     * 修改某个交换机的某个流表内容
+     * 修改某个交换机的某个流表项内容
      * @param switchId 交换机id
      * @param tableId 流表id
+     * @param flowId 流表项id
      * @return 是否成功删除
      */
-    public static boolean modifyFlowTable(String switchId,String tableId){
-        return false;
+    public static boolean modifyFlowTable(String switchId,String tableId,String flowId,OpenFlow openFlow){
+        return HttpSender.put(Address.MODIFY_FLOW[0]+switchId+Address.MODIFY_FLOW[1]+tableId+Address.MODIFY_FLOW[2]+flowId,openFlow);
     }
 }
