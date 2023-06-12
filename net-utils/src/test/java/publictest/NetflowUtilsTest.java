51d2287144dccdcc74edba76300f1707e9fb9b8e
steveevens <3236805730@qq.com>
2023-06-12 20:32:00 +0800

    to(Util):修改请求路径

--- net-utils/src/test/java/publictest/NetflowUtilsTest.java
+++ net-utils/src/test/java/publictest/NetflowUtilsTest.java
@@ -1,6 +1,7 @@
 package publictest;
 
 import org.junit.Test;
+import team.sdn.enums.SFlowStatistic;
 import team.sdn.sdnutils.NetflowUtils;
 import team.sdn.util.HttpSender;
 
@@ -10,14 +11,7 @@ import team.sdn.util.HttpSender;
 public class NetflowUtilsTest {
     @Test
     public void datasourceOfSwitchPortTest(){
-//        System.err.println(NetflowUtils.datasourceOfSwitchPort("ALL"));
-        System.err.println(NetflowUtils.bytesPortInputLastSecond("106"));
-//        System.err.println(NetflowUtils.pktsPortInputLastSecond("100"));
-//        System.err.println(NetflowUtils.bytesPortOutputLastSecond("100"));
-//        System.err.println(NetflowUtils.pktsPortOutputLastSecond("100"));
-//        System.err.println(NetflowUtils.portBandwidthUtilizationLastSecond("100"));
-
-//        System.out.println(HttpSender.get("http://192.168.1.250:8008/metric/192.168.1.250/max:100.ifinoctets/json"));
-//                                         http://192.168.1.250:8008/metric/192.168.1.250/max:100.ifinoctets/json
+        System.err.println(NetflowUtils.bytesPortOutputLastSecond("", SFlowStatistic.SUM));
+        System.err.println(NetflowUtils.bytesPortOutputLastSecond("106", SFlowStatistic.AVG));
     }
 }
