7bf9ce7ef12e7d23af3de42da2feddc90f094dcc
TokisakiKurumi <2105721136@qq.com>
2023-06-13 20:20:08 +0800

    feat:增加字节与包速率流量监控

--- net-traffic/src/main/java/team/sdn/net/traffic/websocket/config/WebSocketConfiguration.java
+++ net-traffic/src/main/java/team/sdn/net/traffic/websocket/config/WebSocketConfiguration.java
@@ -1,12 +1,10 @@
 package team.sdn.net.traffic.websocket.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 import team.sdn.net.traffic.websocket.handler.DeviceHandler;
 import team.sdn.net.traffic.websocket.handler.TrafficHandler;
 import team.sdn.net.traffic.websocket.interceptor.ServerInterceptor;
@@ -24,11 +22,6 @@ public class WebSocketConfiguration implements WebSocketConfigurer {
     private static DeviceHandler deviceHandler;
 
     private static TrafficHandler trafficHandler;
-
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
-    }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
